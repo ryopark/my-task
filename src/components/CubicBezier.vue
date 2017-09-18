89ef21bd219fce91aaba27aa5693d119a60cbe35
KaiKeima <keimakun2011@gmail.com>
2017-09-19 08:09:55 +0900

    add hint

---
+++ src/components/CubicBezier.vue
@@ -0,0 +1,30 @@
+<template lang='pug'>
+.cubic-bezier
+  img(src='../assets/cubic-bezier.svg')
+</template>
+
+<script>
+import $ from 'jquery'
+import 'jquery-ui'
+
+export default {
+  name: 'CubicBezier',
+  props: ['cubicBezier'],
+  computed: {
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.cubic-bezier
+  width 300px
+  height 300px
+  background #fff
+  margin-top 10px
+  display flex
+  align-items center
+  align-contetn center
+  justify-content center
+</style>
