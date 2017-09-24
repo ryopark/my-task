4064df64b1dc55c3019f3f2c6e7cd0ed9507df30
ryopark <dlbia1106@gmail.com>
2017-09-24 15:52:49 -0700

    add two knobs

--- src/components/CubicBezier.vue
+++ src/components/CubicBezier.vue
@@ -1,6 +1,10 @@
 <template lang='pug'>
 .cubic-bezier
-  img(src='../assets/cubic-bezier.svg')
+  .control-area
+    .p1(:style='p1Style')
+    .p2(:style='p2Style')
+    .p3(:style='p3Style')
+    .p4(:style='p4Style')
 </template>
 
 <script>
@@ -11,8 +15,79 @@ export default {
   name: 'CubicBezier',
   props: ['cubicBezier'],
   computed: {
+    position: {
+      get () {
+        const cubicBezier = String(this.cubicBezier)
+        const start = cubicBezier.indexOf('(') + 1
+        const end =  cubicBezier.indexOf(')') 
+        const array = cubicBezier.slice(start, end).split(',')
+        return {
+          p1_x: parseInt(array[0]),
+          p1_y: parseInt(array[1]),
+          p2_x: parseInt(array[2]),
+          p2_y: parseInt(array[3])
+        }
+      },
+      set (val) {
+        this.$emit('update', `cubic-bezier(${val.p1_x},${val.p1_y},${val.p2_x},${val.p2_y})`)
+      }
+
+    },
+    p1Style () {
+      return {
+        top: `${this.position.p1_y}px`,
+        left: `${this.position.p1_x}px`
+      }
+    },
+    p2Style () {
+      return {
+        top: `${this.position.p2_y}px`,
+        left: `${this.position.p2_x}px`
+      }
+    },
+    p3Style () {
+      return {
+        top: '190px',
+        left: '-105px'
+      }
+    },
+    p4Style () {
+      return {
+        top: '-105px',
+        left: '190px'
+      }
+
+    }
   },
   mounted () {
+    $('.cubic-bezier > .control-area > .p1').draggable({
+      start: () => {
+      },
+      drag: (e, ui) => {
+        this.position = {
+          p1_x: ui.position.left,
+          p1_y: ui.position.top,
+          p2_x: this.position.p2_x,
+          p2_y: this.position.p2_y,
+        }
+      },
+      stop: () => {
+      }
+    }),
+    $('.cubic-bezier > .control-area > .p2').draggable({
+      start: () => {
+      },
+      drag: (e, ui) => {
+        this.position = {
+          p2_x: ui.position.left,
+          p2_y: ui.position.top,
+          p1_x: this.position.p1_x,
+          p1_y: this.position.p1_y,
+        }
+      },
+      stop: () => {
+      }
+    })    
   }
 }
 </script>
@@ -27,4 +102,31 @@ export default {
   align-items center
   align-contetn center
   justify-content center
+  .control-area
+    width 100px
+    height 100px
+    position relative
+    .p1,.p2
+      position absolute
+      width 16px
+      height 16px
+      background #A623B8
+      border-radius 50%
+      cursor move
+      box-shadow 0 0 0 0 rgba(166, 35, 184, 0.0)
+      transition box-shadow 0.2s cubic-bezier(0.4, 0.4, 0, 1)
+      &:hover
+        box-shadow 0 0 0 4px rgba(166, 35, 184, 0.2)
+    .p3,.p4
+      position absolute
+      width 16px
+      height 16px
+      background #56365b
+      border-radius 50%
+      cursor move
+      box-shadow 0 0 0 0 rgba(166, 35, 184, 0.0)
+      transition box-shadow 0.2s cubic-bezier(0.4, 0.4, 0, 1)
+      &:hover
+        box-shadow 0 0 0 4px rgba(166, 35, 184, 0.2)
+    
 </style>
